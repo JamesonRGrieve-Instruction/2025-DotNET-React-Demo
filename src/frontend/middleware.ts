9135394d151d131998fca9a029658f101ca09ef3
James Grieve <jamesonrgrieve@gmail.com>
2025-07-15 19:44:07 -0600

    Validate JWT on protected path;

---
+++ src/frontend/middleware.ts
@@ -0,0 +1,24 @@
+import { NextResponse } from "next/server";
+
+function validateJWT(jwt) {
+  return false;
+}
+export default async function Middleware(req) {
+  console.log(req.cookies.get("jwt")?.value);
+  let response = NextResponse.next();
+  if (!validateJWT(req.cookies.get("jwt")?.value)) {
+    const url = req.nextUrl.clone();
+    url.pathname = "/";
+    response = NextResponse.redirect(url);
+    response.cookies.set({
+      name: "jwt",
+      value: "",
+      path: "/",
+      maxAge: 0,
+    });
+  }
+  return response;
+}
+export const config = {
+  matcher: ["/api/:path*", "/weather/:path*"],
+};
